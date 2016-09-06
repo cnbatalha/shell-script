16c80c584f1056917501195287d459274a556e4b
Carlos Nascimento <cnbatalha@gmail.com>
2016-09-06 16:30:10 -0400

    carregando configuracoes

--- learn/config/exemplo.sh
+++ learn/config/exemplo.sh
@@ -7,5 +7,15 @@ EMAIL_SENDER=
 PASSWD_EMAIL_SENDER=
 
 # loop para ler linhas da configuracao
+while read LINHA; do
 
+# Ignorando linhas de comentario
+[ "$(echo $LINHA | cut -c1)" = '#' ] && continue
+
+# Ignorando as linhas em branco
+[ "$LINHA" ] || continue
+
+echo "$LINHA"
+
+done< "$CONFIG"
 
