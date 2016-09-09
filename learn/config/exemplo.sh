6dd86a1d80a7d30605b55c9c5a6ae8b9f23759ac
Carlos Nascimento <cnbatalha@gmail.com>
2016-09-08 21:12:08 -0400

    melhorando interpretacao do arquivo de configuracao

--- learn/config/exemplo.sh
+++ learn/config/exemplo.sh
@@ -9,13 +9,33 @@ PASSWD_EMAIL_SENDER=
 # loop para ler linhas da configuracao
 while read LINHA; do
 
-# Ignorando linhas de comentario
-[ "$(echo $LINHA | cut -c1)" = '#' ] && continue
+	# Ignorando linhas de comentario
+	[ "$(echo $LINHA | cut -c1)" = '#' ] && continue
 
-# Ignorando as linhas em branco
-[ "$LINHA" ] || continue
+	# Ignorando as linhas em branco
+	[ "$LINHA" ] || continue
 
-echo "$LINHA"
+	set - $LINHA
+
+	chave=$1
+	shift
+	valor=$*
+
+	# echo $chave
+	# echo $valor
+
+	case $chave in
+		
+		EMAIL)
+		EMAIL_SENDER=$valor
+		;;
+		PASSWD)
+		PASSWD_EMAIL_SENDER=$valor
+		;;
+	esac
 
 done< "$CONFIG"
+
+echo $EMAIL_SENDER
+echo $PASSWD_EMAIL_SENDER
 
